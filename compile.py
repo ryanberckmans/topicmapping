7afba54dd58933efd4ee942706104bce779aa81d
Andrea Lancichinetti <arg.lanci@gmail.com>
2013-11-05 14:23:34 -0600

    better compiler

--- compile.py
+++ compile.py
@@ -11,8 +11,10 @@ print 'compiling Infomap'
 os.chdir('./Sources/Infomap-0.11.5/')
 os.system('make clean')
 os.system('make')
+print 'leaving directory: ',
 os.system('pwd')
 os.chdir('../..')
+print 'moved back to directory: ',
 os.system('pwd')
 os.system('rm -r bin/')
 os.system('mkdir bin')
@@ -31,6 +33,20 @@ replaced_file.close()
 
 print 'compiling topic mapping'
 os.system('g++ -O3 -funroll-loops -Wall -o bin/topicmap ./Sources/TopicMapping/docmap_tmp.cpp')
+
+print 'Running code with no arguments: '
+os.system('./bin/topicmap')
+
+
+print 'The code has been installed in ', cur_dir
+print 'Please do not rename/move the folder "bin" and its content from', cur_dir
+print 'Since topicmap calls ', infomap_path, ', renaming the executables breaks the code.'
+print 'If you are not happy with the current location,'
+print 'please move the entire folder "topicmapping" to the desired location'
+print 'and run this script again.'
+
+
+
 
 
 
